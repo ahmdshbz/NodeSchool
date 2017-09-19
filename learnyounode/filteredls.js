5667535436ffc31d1f31bf2349582e4b05e98db5
Ahmed Shahbaz <ahmdshbz@gmail.com>
2017-09-19 16:21:37 -0700

    Improve filteredls.js

--- learnyounode/filteredls.js
+++ learnyounode/filteredls.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 fs.readdir(process.argv[2], function callback (err, list) {
   if (err)
-    console.log(err);
+    return console.log(err);
   for (var i = 0; i < list.length; ++i) {
     if (path.extname(list[i]) === ".".concat(process.argv[3]))
       console.log(list[i]);
@@ -10,7 +10,7 @@ fs.readdir(process.argv[2], function callback (err, list) {
 });
 
 /* official solution:
-var fs = require('fs')
+    var fs = require('fs')
     var path = require('path')
     
     var folder = process.argv[2]
