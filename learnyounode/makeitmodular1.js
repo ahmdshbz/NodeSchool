f510d9122e330ed5727032cb84b3b60e86ee041e
Ahmed Shahbaz <ahmdshbz@gmail.com>
2017-09-19 19:41:00 -0700

    6/13

---
+++ learnyounode/makeitmodular1.js
@@ -0,0 +1,14 @@
+module.exports = function myfirstmodule (dir, ext, callback) {
+  var fs = require('fs')
+  var path = require('path')
+  var dotExt = '.' + ext
+  fs.readdir(dir, function (err, files) {
+    if (err)
+      return callback(err)
+    var filtered = files.filter(function (file) {
+      return path.extname(file) === dotExt
+    })
+    callback(err, filtered)
+  }) 
+}
+
