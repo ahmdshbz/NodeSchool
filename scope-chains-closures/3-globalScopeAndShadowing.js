46b7ca3387908582962507b5018003397aae219c
Ahmed Shahbaz <ahmdshbz@gmail.com>
2017-09-23 16:19:22 -0700

    scopes-chains-closures

---
+++ scope-chains-closures/3-globalScopeAndShadowing.js
@@ -0,0 +1,7 @@
+function foo () {
+  quux = 88
+  var bar
+  function zip () {
+    var quux
+  }
+}
---
+++ scope-chains-closures/4-closures.js
@@ -0,0 +1,9 @@
+function foo () {
+  quux = 88
+  var bar
+  function zip () {
+    var quux
+    bar = true
+  }
+  return zip
+}
