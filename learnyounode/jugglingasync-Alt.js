1ae78199eb7a9f2432de0b0bdb99c2144550f818
Ahmed Shahbaz <ahmdshbz@gmail.com>
2017-09-23 21:27:50 -0700

    9/13 alternative solution

--- learnyounode/jugglingasync-Alt.js
+++ learnyounode/jugglingasync-Alt.js
@@ -8,16 +8,16 @@ sources.forEach(processUrl);
 
 function processUrl (source, index) {
   http.get(sources[index], function (response) {
-    response.pipe(bl(processBuffer));
+    response.pipe(bl((err, data) => processBuffer(err, data, index)));
   });
 }
 
-function processBuffer (error, data) {
+function processBuffer (error, data, index) {
   if (error) {
     return console.error(error);
   }
   content[index] = data.toString();
-  if (++count == sources.length) {
+  if (++count === sources.length) {
     content.forEach((el) => console.log(el));
   }
 }
